7082ce5096a1799febeb72517a65a4ae3dd638bc
Dmitry Andronov <dabealu@gmail.com>
2020-03-29 17:45:14 +0800

    update compose, add prom-server

--- switch-leaderserves.sh
+++ switch-leaderserves.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-VALUE=$( awk -F '=' '/leaderServes/ {print $2}' zoo-conf/zoo.cfg.1 )
+VALUE=$( awk -F '=' '/leaderServes/ {print $2}' conf/zoo.cfg.1 )
 
 if [ "$VALUE" == 'no' ]; then
     VALUE='yes'
@@ -9,7 +9,7 @@ else
 fi
 
 for I in {1..3}; do
-    sed -i '' "s/leaderServes=.*$/leaderServes=${VALUE}/" zoo-conf/zoo.cfg.${I}
+    sed -i '' "s/leaderServes=.*$/leaderServes=${VALUE}/" conf/zoo.cfg.${I}
 done
 
 echo "leaderServes changed to '${VALUE}'"
